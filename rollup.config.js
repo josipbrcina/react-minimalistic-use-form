8feb086d8e3a613ca306a6b58b2dd65cb92efe49
Josip Brcina <josipbrchina@gmail.com>
2020-06-19 18:37:20 +0200

    Fix lint errors

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
-import url from "@rollup/plugin-url";
+import url from '@rollup/plugin-url';
 
 export default [
   {
