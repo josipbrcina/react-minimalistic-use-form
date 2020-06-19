620ab8ae5c21f8d7ecf248a55fd86a13c2dd16c0
Josip Brcina <josipbrchina@gmail.com>
2020-06-19 18:36:36 +0200

    Fix rollup export

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,47 @@
 import babel from '@rollup/plugin-babel';
-import pkg from './package.json';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
+import url from "@rollup/plugin-url";
 
-export default {
-  input: 'src/lib/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-  ],
-  external: Object.keys(pkg.dependencies),
-  plugins: [
-    babel({
-      babelHelpers: 'bundled',
-    }),
-  ],
-};
+export default [
+  {
+    input: 'src/lib/index.js',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        sourcemap: false,
+        globals: { react: 'React' },
+      },
+      {
+        file: 'dist/index.es.js',
+        format: 'es',
+        sourcemap: false,
+      },
+    ],
+    plugins: [
+      resolve({
+        preferBuiltins: true,
+      }),
+      external({
+        includeDependencies: true,
+      }),
+      url(),
+      json(),
+      babel({
+        plugins: [
+          '@babel/plugin-proposal-object-rest-spread',
+          '@babel/plugin-proposal-optional-chaining',
+          '@babel/plugin-syntax-dynamic-import',
+          '@babel/plugin-proposal-class-properties',
+          'babel-plugin-transform-react-remove-prop-types',
+        ],
+        exclude: 'node_modules/**',
+      }),
+      commonjs(),
+    ],
+    external: ['react', 'react-dom'],
+  },
+];
