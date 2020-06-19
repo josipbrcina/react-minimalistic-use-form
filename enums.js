1bb739973b9a4a751b6aa3285de8a5552d1cd862
Josip Brcina <josip.brcina@finleap.com>
2020-06-19 10:37:13 +0200

    Merge state and handle with useReducer to avoid multiple re-renders and to avoid unnecessary useEffects to handle initial values, errors

---
+++ enums.js
@@ -0,0 +1,18 @@
+export const elementTypes = {
+  checkbox: 'checkbox',
+  radio: 'radio',
+  textarea: 'textarea',
+};
+
+export const htmlAttributes = {
+  value: 'value',
+  checked: 'checked',
+  for: 'for',
+};
+
+export const eventTypes = {
+  change: 'change',
+  click: 'click',
+  input: 'input',
+  blur: 'blur',
+};
