d60c88de7f60aa0fae9d7b4c01e525d3dc03ce42
Josip Brcina <josipbrchina@gmail.com>
2022-01-28 13:30:47 +0100

    Update validatePlugins utility tests with validate pluging test cases

--- src/utils/__tests__/validatePlugins.test.ts
+++ src/utils/__tests__/validatePlugins.test.ts
@@ -4,14 +4,27 @@ import { noop } from '../noop';
 describe('validatePlugins utility function', () => {
   it('Should throw an error if scrollToError is not a function', () => {
     // @ts-ignore
-    expect(() => validatePlugins({ scrollToError: 'string' })).toThrow('Plugin scrollToError must be a type of function!');
+    expect(() => validatePlugins({ scrollToError: 'string' })).toThrow('Plugin "scrollToError" must be a type of function!');
   });
 
   it('Should not throw if scrollToError is a function', () => {
-    expect(() => validatePlugins({ scrollToError: noop })).not.toThrow('Plugin scrollToError must be a type of function!');
+    expect(() => validatePlugins({ scrollToError: noop })).not.toThrow('Plugin "scrollToError" must be a type of function!');
   });
 
   it('Should not throw if scrollToError is not defined', () => {
-    expect(() => validatePlugins({})).not.toThrow('Plugin scrollToError must be a type of function!');
+    expect(() => validatePlugins({})).not.toThrow('Plugin "scrollToError" must be a type of function!');
+  });
+
+  it('Should throw an error if validate is not a function', () => {
+    // @ts-ignore
+    expect(() => validatePlugins({ validate: 'string' })).toThrow('Plugin "validate" must be a type of function!');
+  });
+
+  it('Should not throw if validate is a function', () => {
+    expect(() => validatePlugins({ validate: noop })).not.toThrow('Plugin "validate" must be a type of function!');
+  });
+
+  it('Should not throw if validate is not defined', () => {
+    expect(() => validatePlugins({})).not.toThrow('Plugin "validate" must be a type of function!');
   });
 });
