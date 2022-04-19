23b16b52b1388f086c1e87a14f61b08c47adf5df
Josip Brcina <josipbrchina@gmail.com>
2022-04-19 16:34:18 +0200

    Remove unused useEffect import declaration

--- src/stories/Form.stories.tsx
+++ src/stories/Form.stories.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control  */
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useForm, Form, IonSubmitResponse } from '../lib';
 import '../style.css';
 import { renderFieldErrors } from './renderFieldErrors';
